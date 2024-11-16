72cae763eddd5aedfaa5b8f92daad898a10d421d
jhSong915 <105760647+jhSong915@users.noreply.github.com>
2024-11-16 23:13:33 +0900

    Add files via upload

---
+++ FileTransferClient/FTPClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileTransferClient
+{
+    public class FTPClient
+    {
+        public object _ftp_ip { get; private set; }
+        public object _ftp_port { get; private set; }
+        public object _ftp_userId { get; private set; }
+        public object _ftp_password { get; private set; }
+
+        public FTPClient(object ftp_ip, object ftp_port, object ftp_userId, object ftp_password)
+        {
+            _ftp_ip = ftp_ip;
+            _ftp_port = ftp_port;
+            _ftp_userId = ftp_userId;
+            _ftp_password = ftp_password;
+        }
+
+        public bool ConnectToServer()
+        {
+            string url = $"ftp://{_ftp_ip}:{_ftp_port}/";
+            try
+			{
+                FtpWebRequest _ftpWebRequest = (FtpWebRequest)WebRequest.Create(url);
+                _ftpWebRequest.Credentials = new NetworkCredential((string)_ftp_userId, (string)_ftp_password);
+                _ftpWebRequest.KeepAlive = false;
+                _ftpWebRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                _ftpWebRequest.UsePassive = true;
+
+                using (FtpWebResponse _response =(FtpWebResponse)_ftpWebRequest.GetResponse())
+                {
+                    return true;
+                }
+            }
+			catch (Exception ex)
+			{
+                MessageBox.Show($"Failed to Connect Server {_ftp_ip}:{_ftp_port} - {ex.Message}");
+                return false;
+			}
+        }
+
+        public bool UploadFile(string localFilePath, string remoteFilePath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(localFilePath);
+                string url = $"FTP://{this._ftp_ip}:{this._ftp_port}/{remoteFilePath}";
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+                request.Method = WebRequestMethods.Ftp.UploadFile;
+                request.Credentials = new NetworkCredential((string)this._ftp_userId, (string)this._ftp_password);
+
+                using (FileStream fileStream = fileInfo.OpenRead())
+                using (Stream requestStream = request.GetRequestStream())
+                {
+                    fileStream.CopyTo(requestStream);
+                }
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse()) { }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to upload file {remoteFilePath}: {ex.Message}");
+                return false;
+            }
+        }
+
+        public bool DownloadFile(string remoteFilePath, string localFilePath)
+        {
+            try
+            {
+                string url = $"FTP://{this._ftp_ip}:{this._ftp_port}/{remoteFilePath}".Replace("\\", "/");
+
+                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(url);
+                request.Method = WebRequestMethods.Ftp.DownloadFile;
+                request.Credentials = new NetworkCredential((string)this._ftp_userId, (string)this._ftp_password);
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                using (Stream responseStream = response.GetResponseStream())
+                using (FileStream fileStream = new FileStream(localFilePath, FileMode.Create))
+                {
+                    responseStream.CopyTo(fileStream);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to download file {remoteFilePath}: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ FileTransferClient/SFTPClient.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+
+namespace FileTransferClient
+{
+    public class SFTPClient
+    {
+        public SftpClient _sftp_conn { get; private set; }
+        public object _sftp_ip { get; private set; }
+        public object _sftp_port { get; private set; }
+        public object _sftp_userId { get; private set; }
+        public object _sftp_password { get; private set; }
+
+        public SFTPClient(object sftp_ip, object sftp_port, object sftp_userId, object sftp_password)
+        {
+            _sftp_ip = sftp_ip;
+            _sftp_port = sftp_port;
+            _sftp_userId = sftp_userId;
+            _sftp_password = sftp_password;
+            ConnectToServer();
+        }
+        
+        ~SFTPClient() {
+            try
+            {
+                if ( _sftp_conn != null )
+                {
+                    _sftp_conn.Disconnect();
+                    _sftp_conn.Dispose();
+                }
+            }
+            finally
+            {
+                _sftp_ip = null;
+                _sftp_port = null;
+                _sftp_userId = null;
+                _sftp_password = null;
+                _sftp_conn = null;
+            }
+        }
+
+        public bool ConnectToServer()
+        {
+            try
+            {
+                _sftp_conn = new SftpClient((string)_sftp_ip, (int)_sftp_port, (string)_sftp_userId, (string)_sftp_password);
+                _sftp_conn.Connect();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to Connect Server {_sftp_ip}:{_sftp_port} - {ex.Message}");
+                return false;
+            }
+        }
+
+        public void Download(string remotePath, string localPath, string orifilename)
+        {
+            DirectoryInfo di = new DirectoryInfo(localPath);
+            if (di.Exists == false)
+            {
+                Directory.CreateDirectory(localPath);
+            }
+                
+            IEnumerable<SftpFile> files = (IEnumerable<SftpFile>)_sftp_conn.ListDirectory(remotePath);
+            foreach (SftpFile file in files)
+            {
+                if (file.Name.Equals(orifilename))
+                {
+                    if ((file.Name != ".") && (file.Name != ".."))
+                    {
+                        string sourceFilePath = remotePath + "/" + file.Name;
+                        string destFilePath = Path.Combine(localPath, file.Name);
+                        string dateFilePath = localPath + "/" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + file.Name.Split('.')[0]; 
+
+                        using (Stream fileStream = File.Create(destFilePath))
+                        {
+                            _sftp_conn.DownloadFile(sourceFilePath, fileStream);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Upload(string localPath, string remotePath, string orifilename)
+        {
+            IEnumerable<FileSystemInfo> infos = new DirectoryInfo(localPath).EnumerateFileSystemInfos();
+            foreach (FileSystemInfo info in infos)
+            {
+                using (Stream fileStream = new FileStream(info.FullName, FileMode.Open))
+                {
+                    if (!_sftp_conn.Exists(remotePath))
+                    {
+                        _sftp_conn.CreateDirectory(remotePath);
+                    }
+                    _sftp_conn.UploadFile(fileStream, remotePath + "/" + info.Name);
+                }
+            }
+        }
+    }
+}
